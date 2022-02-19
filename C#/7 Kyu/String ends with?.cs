dff355b9405b249abd3e1307fef34ca19b592a80
Lumi <shaicloud@gmail.com>
2022-02-19 02:43:31 -0500

    Create String ends with?.cs

---
+++ C#/7 Kyu/String ends with?.cs
@@ -0,0 +1,8 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+public class Kata
+{
+    public static bool Solution(string str, string ending)
+    {
+        return str.EndsWith(ending);
+    }
+}
