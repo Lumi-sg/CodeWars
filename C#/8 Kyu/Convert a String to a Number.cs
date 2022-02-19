3393a648b03818c2c0896d66489ba1612f268288
Lumi <shaicloud@gmail.com>
2022-02-19 00:28:57 -0500

    Create Convert a String to a Number.cs

---
+++ C#/8 Kyu/Convert a String to a Number.cs
@@ -0,0 +1,11 @@
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+using System;
+public class Kata
+{
+    public static int StringToNumber(string str)
+    {
+        return int.Parse(str);
+        
+    }
+}
