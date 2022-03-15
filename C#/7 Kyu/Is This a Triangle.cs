da8b546fab1baeb4c70a3f915428f21a575d2ae1
Lumi <shaicloud@gmail.com>
2022-03-15 19:18:06 -0400

    Create Is This a Triangle.cs

---
+++ C#/7 Kyu/Is This a Triangle.cs
@@ -0,0 +1,14 @@
+public class Triangle
+{
+    public static bool IsTriangle(int a, int b, int c)
+    {
+      if (a + b + c > 0)
+        {
+        return true;
+        }
+      else
+        {
+        return false;
+        }
+    }
+}
