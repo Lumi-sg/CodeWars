97a7ed515b6de2b5e919bd07a2582ac0ec2b6e76
Lumi <shaicloud@gmail.com>
2022-04-09 04:30:31 -0400

    Update 20. Valid Parentheses.cs

--- C#/LeetCode/20. Valid Parentheses.cs
+++ C#/LeetCode/20. Valid Parentheses.cs
@@ -1,32 +1,32 @@
 public class Solution {
-    public bool IsValid(string s) 
-    {
-        Dictionary<char, char> dictionary = new Dictionary<char, char>();
-            Stack<char> stack = new Stack<char>();
-
-            dictionary.Add(')', '(');
-            dictionary.Add('}', '{');
-            dictionary.Add(']', '[');
-
-            foreach (var str in s)
+    public bool IsValid(string s) {
+        if (s == null || s == string.Empty)
+            return true;
+        
+        Dictionary<char, char> charDict = new Dictionary<char, char>();
+        Stack<char> stack = new Stack<char>();
+        
+        charDict.Add(')', '(');
+        charDict.Add('}', '{');
+        charDict.Add(']', '[');
+        
+        foreach (var c in s)
+            if (c == ')' || c == '}' || c == ']')
             {
-                if (str is ')' or ']' or '{')
+                if (stack.Count > 0 && stack.Peek() == charDict[c])
                 {
-                    if (stack.Count > 0 && stack.Peek() == dictionary[str])
-                    {
-                        stack.Pop();
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    stack.Pop();
                 }
                 else
                 {
-                    stack.Push(str);
+                    return false;
                 }
             }
-
-            return stack.Count == 0;
+            else
+            {
+                stack.Push(c);
+            }
+        
+        return stack.Count == 0;
     }
 }
