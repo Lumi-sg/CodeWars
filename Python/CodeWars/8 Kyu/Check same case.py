99d6dd8937efcaf6ba9af210b267ccbebfd191a1
Lumi <shaicloud@gmail.com>
2022-04-09 22:33:10 -0400

    Create Check same case.py

--- Python/CodeWars/8 Kyu/Check same case.py
+++ Python/CodeWars/8 Kyu/Check same case.py
@@ -2,7 +2,7 @@ def same_case(a, b):
     if a.isalpha() and b.isalpha():
         if a.islower() and b.islower() or a.isupper() and b.isupper():
             return 1
-        elif a.islower() and b.isupper() or a.isupper() and b.islower():
+        else:
             return 0
     else:
         return -1
